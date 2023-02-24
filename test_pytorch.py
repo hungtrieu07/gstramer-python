1ec9a9cb76bcb2f8a7936252d8101d6395cc9aa7
Trieu Viet Hung <69461968+hungtrieu07@users.noreply.github.com>
2023-02-24 17:07:31 +0700

    Add files via upload

--- test_pytorch.py
+++ test_pytorch.py
@@ -1,43 +1,38 @@
-import os, sys
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GObject
+gi.require_version('GObject', '2.0')
+from gi.repository import Gst
 import numpy as np
 import torch
 from PIL import Image
 import cv2
 
-frame_format, pixel_bytes = 'RGBA', 4
-
 Gst.init(None)
-# pipeline = Gst.parse_launch(f'''
-#     filesrc location=./video/PEDESTRIAN/KM77000.VDS.CAM.17_10.2.163.76_20221227T150312_PEDESTRIAN_1_ACCEPTED.avi num-buffers=200 !
-#     decodebin !
-#     videoconvert !
-#     autovideosink name = s
-# ''')
-
-pipeline = Gst.Pipeline.new('new-pipeline')
 
 src = Gst.ElementFactory.make('filesrc', 'source')
-src.set_property('location', './video/test.mp4')
-src.set_property('num-buffers', 200)
+src.set_property('location', 'video/test.mp4')
+src.set_property('num-buffers', 20000)
+src.set_property('blocksize', 4096)
 decodebin = Gst.ElementFactory.make('decodebin', 'decoder')
 videoconvert = Gst.ElementFactory.make('videoconvert', 'converter')
+queue = Gst.ElementFactory.make('queue', 'queue')
 autovideosink = Gst.ElementFactory.make('autovideosink', 's')
 
-s = pipeline.get_by_name('s')
+pipeline = Gst.Pipeline.new('new-pipeline')
 
 pipeline.add(src)
 pipeline.add(decodebin)
 pipeline.add(videoconvert)
+pipeline.add(queue)
 pipeline.add(autovideosink)
 
 def on_pad_added(element, pad):
     sinkpad = videoconvert.get_static_pad('sink')
     pad.link(sinkpad)
+    decodebin.link(queue)
+    queue.link(autovideosink)
     sinkpad = autovideosink.get_static_pad('sink')
-    videoconvert.link(autovideosink)
+    videoconvert.link(queue)
     
 src.link(decodebin)
 decodebin.connect('pad-added', on_pad_added)
@@ -45,41 +40,38 @@ decodebin.connect('pad-added', on_pad_added)
 detector = torch.hub.load('ultralytics/yolov5', 'custom', 'yolov5s.pt').eval().to(torch.device('cpu'))
 
 def on_frame_probe(pad, info):
-    # sample = s.emit('pull-sample')
     buf = info.get_buffer()
     print(f'[{buf.pts / Gst.SECOND:6.2f}]')
     image_tensor = buffer_to_image_tensor(buf, pad.get_current_caps())
-    # print(image_tensor.size)
     with torch.no_grad():
         detections = detector(image_tensor)
+        print(detections)
         objects = (detections.xyxy[0]).tolist()
         img_np = np.array(image_tensor)
         for i in range(len(objects)):
             x_min, y_min, x_max, y_max = int(objects[i][0]), int(objects[i][1]), int(objects[i][2]), int(objects[i][3])
             cv2.rectangle(img_np, (x_min, y_min), (x_max, y_max), (255,0,0), 2)
-        # print(img_np.shape)
-        
-    new_data = img_np.tobytes()
-    new_buf = Gst.Buffer.new_wrapped(new_data)
-    sample_new = Gst.Sample.new(new_buf, pad.get_current_caps())
-    s.emit('push-sample', sample_new)
-        
+            # new_data = img_np.tobytes()
+            # new_buf = Gst.Buffer.new_wrapped(new_data)
+        # Save the first frame as an image
+        Image.fromarray(img_np[:,:,:3]).save("output.jpg")
+        # Remove the probe from the pad to stop processing any more frames
+        # pad.remove_probe(info.id)
     return Gst.PadProbeReturn.OK
+
 
 def buffer_to_image_tensor(buf, caps):
     caps_structure = caps.get_structure(0)
+    # print(caps_structure.to_string())
     height, width = caps_structure.get_value('height'), caps_structure.get_value('width')
-    # print(height, width)
+    channels = 3
     is_mapped, map_info = buf.map(Gst.MapFlags.READ)
     if is_mapped:
         try:
-            # image_array = np.ndarray(
-            #     (height, width, pixel_bytes),
-            #     dtype=np.uint8,
-            #     buffer=map_info.data
-            # ).copy() # extend array lifetime beyond subsequent unmap
-            image_array=np.frombuffer(map_info.data ,dtype=np.uint8).reshape(540, width,3)
-            # print(image_array.shape)
+            image_array = np.frombuffer(map_info.data ,dtype=np.uint8).reshape((640, 1637, 3)).copy()
+            image_array.resize((height, width, channels))
+            image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+            image_array = cv2.resize(image_array, (1920, 1080))
             return Image.fromarray(image_array[:,:,:3]) # RGBA -> RGB
         finally:
             buf.unmap(map_info)
